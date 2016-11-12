db160f7b4223a0fb6ac2a10c160ea4509726ff31
Ogadent <OgadentCoC@gmail.com>
2016-11-12 16:22:51 +0100

    Update 12e.c

--- blatt3/aufgabe12/12e.c
+++ blatt3/aufgabe12/12e.c
@@ -5,7 +5,7 @@ int main()
         int x, result;
         char c;
         result = scanf("%i%c", &x, &c);
-        if((result != 2 || getchar() != '\n') && (x > 0 && c > 96 && c < 123)){
+        if((result != 2 || getchar() != '\n') || x <= 0 || c < 97 || c > 122)){
                 printf("Die Eingabe war nicht erfoglreich.");
         }else{
                 printf("Die Eingabe war erfoglreich.");
