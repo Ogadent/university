20817ad6108c4e7e6dc3942dc203d00e156c7e5f
Ogadent <OgadentCoC@gmail.com>
2016-11-12 14:32:55 +0100

    Create 12d.c

---
+++ blatt3/aufgabe12/12d.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+        int x, result;
+        result = scanf("%i", &x);
+        if((!result || getchar() != '\n') && (x >= 1 && x <= 12)){
+                printf("Die Eingabe war nicht erfoglreich.");
+        }else{
+                printf("Die Eingabe war erfoglreich.");
+        }
+        return 0;
+}
