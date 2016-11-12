90d9dc5bf640f60bb3c93229a7e76dce0f0ca2f4
Ogadent <OgadentCoC@gmail.com>
2016-11-12 16:22:02 +0100

    Update 12d.c

--- blatt3/aufgabe12/12d.c
+++ blatt3/aufgabe12/12d.c
@@ -4,7 +4,7 @@ int main()
 {
         int x, result;
         result = scanf("%i", &x);
-        if((!result || getchar() != '\n') && (x >= 1 && x <= 12)){
+        if((!result || getchar() != '\n') || x < 1 || x > 12){
                 printf("Die Eingabe war nicht erfoglreich.");
         }else{
                 printf("Die Eingabe war erfoglreich.");
