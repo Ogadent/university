793184f824079ac11920045dde7548f8b179423e
Ogadent <OgadentCoC@gmail.com>
2016-11-18 12:12:41 +0100

    Create 16e.c

---
+++ blatt4/aufgabe16/16e.c
@@ -0,0 +1,8 @@
+void print_rectangle(int lines, int columns)
+{
+        int i, j;
+        for(i = 0; i < lines; i++){
+                for(j = 0; j < columns; j++) printf("0");
+                printf("\n");
+        }
+}
