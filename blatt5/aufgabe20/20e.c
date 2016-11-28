8639484a15dfbc459db88259d75c64b9ff7cf03e
JFCbam <jfcbam@googlemail.com>
2016-11-28 17:20:58 +0100

    Update 20e.c

--- blatt5/aufgabe20/20e.c
+++ blatt5/aufgabe20/20e.c
@@ -3,8 +3,10 @@ int aufsteigend_sortiert(int w[], int s)
 	int li = 0, re = s + 1, m;
 	while (li < re - 1) {
 		m = (li + re) / 2;
-		if (s <= w[m]) re = m;
-		else li = m;
+		if (s <= w[m]){
+		re = m;
+		}else{ li = m;
+		     }
 	}
 	return re;
 }
