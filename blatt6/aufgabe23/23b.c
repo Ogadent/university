3d1337051928868cbc6336f7d3504148664ce68f
Ogadent <OgadentCoC@gmail.com>
2016-12-03 22:54:05 +0100

    Create 23b.c

---
+++ blatt6/aufgabe23/23b.c
@@ -0,0 +1,9 @@
+#ifndef AUFGABE23_H
+#define AUFGABE23_H
+
+extern int starts_with(char[], char[]);
+extern void delete(char[], int);
+extern void count(char[], int[]);
+extern void reverse(char[], char[]);
+
+#endif AUFGABE23_H
