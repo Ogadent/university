72f3d36480d3ce686a4d0e15f6c472d344315f65
Ogadent <OgadentCoC@gmail.com>
2016-12-04 17:25:10 +0100

    Update 24a.c

--- blatt6/aufgabe24/24a.c
+++ blatt6/aufgabe24/24a.c
@@ -3,6 +3,5 @@ double read_double(void)
         double d;
         int result = scanf("%lf", &d);
         if (result == 1 && getchar() == '\n') return d;
-        flush_buff();
-        return !result ? DBL_MIN : DBL_MAX;
+        return !(result = flush_buff()) ? DBL_MIN : DBL_MAX;
 }
