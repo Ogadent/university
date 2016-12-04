cd9bc5ce19719762ab39a952578eb5fa58dc38f5
Ogadent <OgadentCoC@gmail.com>
2016-12-04 23:21:05 +0100

    Update 24b.c

--- blatt6/aufgabe24/24b.c
+++ blatt6/aufgabe24/24b.c
@@ -1,10 +1,10 @@
 int read_three_chars(void)
 {
-        char s[4] = "000";
+        char s[4] = "***";
         int i, n;
         for (i = 0; i < 3; i++) {
-                if (n = getchar()) s[i] = n;
-                else return flush_buff() - 1;
+                if ((n = getchar()) != EOF) s[i] = n;
+                else return EOF;
         }
-        return (getchar() != '\n') - 3;
+        return getchar() == '\n' ? 1 : flush_buff() + EOF;
 }
