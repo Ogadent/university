a73d718196d3645676a793b3f109c48c5d185760
Ogadent <OgadentCoC@gmail.com>
2016-12-04 23:26:15 +0100

    Update 24c.c

--- blatt6/aufgabe24/24c.c
+++ blatt6/aufgabe24/24c.c
@@ -1,9 +1,8 @@
 int read_num(void)
 {
-        int i, result;
-        result = scanf("%i", &i);
+        int i, result = scanf("%i", &i);
         if (result == 1 && getchar() == '\n') {
                 return  i > 0 && i < 13 ? i : 0;
         }
-        return !result ? -1 : flush_buff() - 2;
+        return result ? EOF : flush_buff() + EOF;
 }
