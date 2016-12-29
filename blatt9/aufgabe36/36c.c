e255782b8f987863226b3ae477de7f57989f32e2
Ogadent <OgadentCoC@gmail.com>
2016-12-29 18:04:22 +0100

    Create 36c.c

---
+++ blatt9/aufgabe36/36c.c
@@ -0,0 +1,11 @@
+char *strrchr(const char *s, int c)
+{
+        const char *last = NULL;
+        do
+        {
+                if (*s == (char)c)
+                last = s;
+        }
+        while (*s++);
+        return (char *)last;
+}
