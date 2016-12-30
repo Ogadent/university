b27b8b5b723556b798b621df909194bc1415e327
Ogadent <OgadentCoC@gmail.com>
2016-12-31 00:16:15 +0100

    Update 36a.c

--- blatt9/aufgabe36/36a.c
+++ blatt9/aufgabe36/36a.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-void remove_all_chars(char* str, char c);
-
 int main()
 {
     char satz[] = "Die naechste Vorlesung ist am 9.1.2017, 10.00 Uhr, im HS 1 (Bau C)";
-    char delimiter[] = " ";
+    char delimiter[] = " ,()";
     char* ptr = strtok(satz, delimiter);
 
     while (ptr != NULL) {
-        remove_all_chars(ptr, ',');
-        remove_all_chars(ptr, '(');
-        remove_all_chars(ptr, ')');
-        printf("Abschnitt gefunden: %s\n", ptr);
+        printf("%s\n", ptr);
         ptr = strtok(NULL, delimiter);
     }
     return 0;
-}
-
-void remove_all_chars(char* str, char c) {
-    char *pr = str, *pw = str;
-    while (*pr) {
-        *pw = *pr++;
-        pw += (*pw != c);
-    }
-    *pw = '\0';
 }
