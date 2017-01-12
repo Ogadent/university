0702a17dcf5826ec37faa9767d98cf1f5987fa3a
Ogadent <OgadentCoC@gmail.com>
2017-01-12 14:14:52 +0100

    Create 39a.c

---
+++ blatt10/aufgabe39/39a.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+int safecpy(char **t, char *s)
+{
+        size_t size1, size2;
+        size1 = strlen(*t);
+        size2 = strlen(s);
+        if (size1 > ++size2) {
+                strcpy(*t, s);
+                return 1;
+        }
+        *t = calloc(size2, sizeof(*s));
+        if (*t)
+                strcpy(*t, s);
+        return *t;
+}
